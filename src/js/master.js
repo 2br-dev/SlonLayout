4e3a8a2e49a1165f7508d8395df635dbd9f52b59
2br-dev <masterkadaj@gmail.com>
2021-02-20 18:53:30 +0300

    Контакты

--- src/js/master.js
+++ src/js/master.js
@@ -143,3 +143,112 @@ function init(){
     });
     
 }
+
+//= Асинхронная загрузка скриптов =========================================
+loadScript = (url, callback) => {
+
+	var script = document.createElement("script")
+	script.type = "text/javascript";
+
+	if (script.readyState){  //IE
+		script.onreadystatechange = function(){
+			if (script.readyState == "loaded" ||
+					script.readyState == "complete"){
+				script.onreadystatechange = null;
+				callback();
+			}
+		};
+	} else {  //Others
+		script.onload = function(){
+			callback();
+		};
+	}
+
+	script.src = url;
+	document.getElementsByTagName("head")[0].appendChild(script);
+}
+
+//= Вызов инициализации карты =============================================
+if($('#map').length){
+    loadScript("https://cdn.jsdelivr.net/gh/openlayers/openlayers.github.io@master/en/v6.4.3/build/ol.js", () => {
+        initMap();
+    })
+}
+
+//= Инициализация карты ===================================================
+function initMap(){
+
+    var coords = [37.7400938, 44.7267618];
+    
+    var style = new ol.style.Style({
+        image: new ol.style.Icon({
+            anchor: [.5, 1],
+            src: '/img/map_marker.png'
+        })
+    });
+    
+    var marker = new ol.Feature({
+        type: 'icon',
+        geometry: new ol.geom.Point(ol.proj.fromLonLat(coords))
+    })
+
+    var vectorSource = new ol.source.Vector({
+        features: [marker]
+    })
+
+    var vectorLayer = new ol.layer.Vector({
+		source: vectorSource,
+        style: style
+	});
+
+    var map = new ol.Map({
+        target: 'map',  // The DOM element that will contains the map
+        interactions: ol.interaction.defaults({mouseWheelZoom:false}), //Disable scroll event
+		renderer: 'canvas', // Force the renderer to be used
+		layers: [
+			// Add a new Tile layer getting tiles from OpenStreetMap source
+			new ol.layer.Tile({
+				source: new ol.source.OSM({
+					url: "https://basemaps.cartocdn.com/rastertiles/light_all/{z}/{x}/{y}{r}.png"
+				})
+			}),
+			vectorLayer
+		],
+		// Create a view centered on the specified location and zoom level
+		view: new ol.View({
+			center: ol.proj.fromLonLat(coords),
+			zoom: 16
+		})
+    });  
+    
+    // Эвент по клику на маркере
+    map.on('click', function(evt) {
+        var f = map.forEachFeatureAtPixel(
+            evt.pixel,
+            function(ft, layer){return ft;}
+        );
+        if (f && f.get('type') == 'icon') {
+
+            var url = "https://yandex.ru/maps/?pt=";
+            url += coords[0]+","
+                + coords[1]+"&"
+                + "z=17&l=map";
+
+            var linkEl = $('<a href="'+url+'" target="_blank">Google</a>');
+            $('#map').append(linkEl);
+            linkEl[0].click();
+            $(linkEl).remove();
+        }        
+    });
+
+    map.on("pointermove", function (evt) {
+        var hit = this.forEachFeatureAtPixel(evt.pixel, function(feature, layer) {
+            return true;
+        }); 
+        if (hit) {
+            this.getTargetElement().style.cursor = 'pointer';
+        } else {
+            this.getTargetElement().style.cursor = '';
+        }
+    });
+}
